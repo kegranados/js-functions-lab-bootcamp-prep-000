cf64095772da4cdf2a828f51049ff9ade0ad9604
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-20 23:12:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo (name) {
 function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown (days, holiday) {
+function holidayCountdown (holiday, holiday) {
   return `It's ${days} days until ${holiday}!`
 }
 
